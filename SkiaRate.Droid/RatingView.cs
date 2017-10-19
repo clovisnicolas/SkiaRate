8efd0be202f50632823558d0e75e4bfc2d4eae3b
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-19 20:01:21 +0200

    start implementing for android

---
+++ SkiaRate.Droid/RatingView.cs
@@ -0,0 +1,55 @@
+﻿using SkiaSharp.Views.Android;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Android.Content;
+using Android.Runtime;
+using Android.Util;
+
+namespace SkiaRate.Droid
+{
+    public class RatingView : SKCanvasView
+    {
+        public RatingView(Context context) : base(context)
+        {
+            this.PaintSurface += RatingView_PaintSurface;
+        }
+
+        public RatingView(Context context, IAttributeSet attrs) : base(context, attrs)
+        {
+            this.PaintSurface += RatingView_PaintSurface;
+        }
+
+        public RatingView(Context context, IAttributeSet attrs, int defStyleAttr) : base(context, attrs, defStyleAttr)
+        {
+            this.PaintSurface += RatingView_PaintSurface;
+        }
+
+        protected RatingView(IntPtr javaReference, JniHandleOwnership transfer) : base(javaReference, transfer)
+        {
+            this.PaintSurface += RatingView_PaintSurface;
+        }
+
+        private Rating rating;
+
+        public Rating Rating
+        {
+            get { return this.rating; }
+            set
+            {
+                if(this.rating != value)
+                {
+                    this.rating = value;
+                    this.Invalidate();
+                }
+            }
+        }
+
+        private void RatingView_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            this.Rating?.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+        }
+
+    }
+}
