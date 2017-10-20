6c995eb39e072b27c75b803b7a7b511c4870330d
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-20 15:49:42 +0200

    Add forms library

---
+++ SkiaRate.Forms/RatingView.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using SkiaSharp.Views.Forms;
+using Xamarin.Forms;
+
+namespace SkiaRate.Forms
+{
+    public class RatingView : SKCanvasView
+    {
+        public RatingView()
+        {
+            this.BackgroundColor = Color.Transparent;
+            this.PaintSurface += Handle_PaintSurface;
+        }
+
+        public static readonly BindableProperty RatingProperty = BindableProperty.Create(nameof(Rating), typeof(Rating), typeof(RatingView), typeof(Rating));
+
+        public Rating Rating
+        {
+            get { return (Rating)GetValue(RatingProperty); }
+            set { SetValue(RatingProperty, value); }
+        }
+
+        void Handle_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
+        {
+            this.Rating?.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
+        }
+    }
+}
