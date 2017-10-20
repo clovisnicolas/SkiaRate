d31f7d83031718944f52d95e68f6c25a56398327
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-20 12:06:48 +0200

    Add material colors, add path constants

---
+++ SkiaRate.Shared/MaterialColors.cs
@@ -0,0 +1,23 @@
+﻿using SkiaSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiaRate
+{
+    public static class MaterialColors
+    {
+        public static SKColor Red => SKColor.Parse("F44336");
+        public static SKColor Pink => SKColor.Parse("E91E63");
+        public static SKColor Purple => SKColor.Parse("9C27B0");
+        public static SKColor DeepPurple => SKColor.Parse("673AB7");
+        public static SKColor Indigo => SKColor.Parse("3F51B5");
+        public static SKColor Blue => SKColor.Parse("2196F3");
+        public static SKColor Green => SKColor.Parse("4CAF50");
+        public static SKColor Lime => SKColor.Parse("CDDC39");
+        public static SKColor Amber => SKColor.Parse("FFC107");
+        public static SKColor Yellow => SKColor.Parse("FFEB3B");
+        public static SKColor Orange => SKColor.Parse("FF9800");
+
+    }
+}
---
+++ SkiaRate.Shared/PathConstants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkiaRate
+{
+    public static class PathConstants
+    {
+        public const string Star = "M9 11.3l3.71 2.7-1.42-4.36L15 7h-4.55L9 2.5 7.55 7H3l3.71 2.64L5.29 14z";
+        public const string Heart = "M12 21.35l-1.45-1.32C5.4 15.36 2 12.28 2 8.5 2 5.42 4.42 3 7.5 3c1.74 0 3.41.81 4.5 2.09C13.09 3.81 14.76 3 16.5 3 19.58 3 22 5.42 22 8.5c0 3.78-3.4 6.86-8.55 11.54L12 21.35z";
+        public const string Circle = "M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2z";
+    }
+}
