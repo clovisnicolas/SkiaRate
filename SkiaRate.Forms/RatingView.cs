88f94c3ce8e08b85dc3a3dce48ea00b092a555c1
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-21 10:33:02 +0200

    Fix pan on Xamarin.Forms

--- SkiaRate.Forms/RatingView.cs
+++ SkiaRate.Forms/RatingView.cs
@@ -8,11 +8,17 @@ namespace SkiaRate.Forms
 {
     public class RatingView : SKCanvasView
     {
+        private PanGestureRecognizer panGestureRecognizer = new PanGestureRecognizer();
+        private double touchX;
+        private double touchY;
+
         public RatingView()
         {
             this.BackgroundColor = Color.Transparent;
             this.PaintSurface += Handle_PaintSurface;
             this.EnableTouchEvents = true;
+            this.panGestureRecognizer.PanUpdated += PanGestureRecognizer_PanUpdated;
+            this.GestureRecognizers.Add(panGestureRecognizer);
         }
 
         public static readonly BindableProperty RatingProperty = BindableProperty.Create(nameof(Rating), typeof(Rating), typeof(RatingView), default(Rating));
@@ -23,16 +29,32 @@ namespace SkiaRate.Forms
             get { return (Rating)GetValue(RatingProperty); }
             set { SetValue(RatingProperty, value); }
         }
-
+        protected override void OnTouch(SKTouchEventArgs e)
+        {
+            this.touchX = e.Location.X;
+            this.touchY = e.Location.Y;
+            this.Rating.SetValue(touchX, touchY);
+            this.InvalidateSurface();
+        }
         private void Handle_PaintSurface(object sender, SKPaintSurfaceEventArgs e)
         {
             this.Rating?.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
         }
-
-        protected override void OnTouch(SKTouchEventArgs e)
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
         {
-            this.Rating.SetValue(e.Location.X, e.Location.Y);
-            this.InvalidateSurface();
+            var point = ConvertToPixel(new Point(e.TotalX, e.TotalY));
+            if(e.StatusType != GestureStatus.Completed)
+            {
+                this.Rating.SetValue(touchX + point.X, touchY + e.TotalY);
+                this.InvalidateSurface();
+            }
         }
+
+        SKPoint ConvertToPixel(Point pt)
+        {
+            return new SKPoint((float)(this.CanvasSize.Width * pt.X / this.Width),
+                               (float)(this.CanvasSize.Height * pt.Y / this.Height));
+        }
+
     }
 }
