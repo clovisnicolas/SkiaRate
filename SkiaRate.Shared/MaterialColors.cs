ffa09f559d5dc88db10f8998180b67fb204a3528
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-21 11:16:54 +0200

    add missing material colors

--- SkiaRate.Shared/MaterialColors.cs
+++ SkiaRate.Shared/MaterialColors.cs
@@ -13,11 +13,21 @@ namespace SkiaRate
         public static SKColor DeepPurple => SKColor.Parse("673AB7");
         public static SKColor Indigo => SKColor.Parse("3F51B5");
         public static SKColor Blue => SKColor.Parse("2196F3");
+        public static SKColor LightBlue => SKColor.Parse("03A9F4");
+        public static SKColor Cyan => SKColor.Parse("00BCD4");
+        public static SKColor Teal => SKColor.Parse("009688");
         public static SKColor Green => SKColor.Parse("4CAF50");
+        public static SKColor LightGreen => SKColor.Parse("8BC34A");
         public static SKColor Lime => SKColor.Parse("CDDC39");
-        public static SKColor Amber => SKColor.Parse("FFC107");
         public static SKColor Yellow => SKColor.Parse("FFEB3B");
+        public static SKColor Amber => SKColor.Parse("FFC107");
         public static SKColor Orange => SKColor.Parse("FF9800");
+        public static SKColor DeepOrange => SKColor.Parse("FF5722");
+        public static SKColor Brown => SKColor.Parse("795548");
+        public static SKColor Grey => SKColor.Parse("9E9E9E");
+        public static SKColor BlueGrey => SKColor.Parse("607D8B");
+        public static SKColor Black => SKColor.Parse("000000");
+        public static SKColor White => SKColor.Parse("FFFFFF");
 
     }
 }
