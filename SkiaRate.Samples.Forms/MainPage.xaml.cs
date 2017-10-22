a973503bb2cdd2608925daa25fe7715dc5854e30
Clovis Nicolas <clovisnicolas0@gmail.com>
2017-10-22 21:24:24 +0200

    Add extra samples to forms app

--- SkiaRate.Samples.Forms/MainPage.xaml.cs
+++ SkiaRate.Samples.Forms/MainPage.xaml.cs
@@ -12,13 +12,15 @@ namespace SkiaRate.Samples.Forms
             this.ratingView1.Rating = Sample.StarRating;
             this.ratingView2.Rating = Sample.FavRating;
             this.ratingView3.Rating = Sample.CircleRating;
+            this.ratingView4.Rating = Sample.LikeRating;
+            this.ratingView5.Rating = Sample.DislikeRating;
+            this.ratingView6.Rating = Sample.MoviesRating;
+            this.ratingView7.Rating = Sample.ProblemRating;
         }
 
         protected override void OnAppearing()
         {
             base.OnAppearing();
-
-
         }
     }
 }
